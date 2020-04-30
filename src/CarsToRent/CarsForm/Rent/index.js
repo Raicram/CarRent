f8ef4cbfa08fd718e1624bb5612f552bf50e7856
unknown <marcin080599@gmail.com>
2020-04-30 23:42:32 +0200

    changes in rent page

--- src/CarsToRent/CarsForm/Rent/index.js
+++ src/CarsToRent/CarsForm/Rent/index.js
@@ -1,12 +1,91 @@
 import React from 'react';
 import {Field,Form} from 'react-final-form';
+import {FormDiv} from './styles';
 
 const RentForm = props =>{
     console.log(props.match.params.id);
+
+    const onSubmit = values =>{
+       console.log(values);
+    };
+
     return(
-        <div>
+        <FormDiv>
             <h1>Your car: {props.match.params.id}</h1>
-        </div>
+            <div>
+                <Form
+                    onSubmit={onSubmit}
+                    validate={values => {
+                        const errors = {};
+                        if (!values.name) {
+                            errors.name = 'Required'
+                        }
+                        if (!values.surname) {
+                            errors.surname = 'Required'
+                        }
+                        if (!values.firstdate) {
+                            errors.firstdate = 'Required'
+                        }
+                        if (!values.seconddate) {
+                            errors.seconddate = 'Required'
+                        }
+                        return errors
+                    }}
+                    render={({ handleSubmit, form, submitting, pristine, values }) => (
+                        <form onSubmit={handleSubmit}>
+                            <Field name="name">
+                                {({ input, meta }) => (
+                                    <div>
+                                        <label>Name</label>
+                                        <input {...input} type="text" placeholder="Name" />
+                                        {meta.error && meta.touched && <span>{meta.error}</span>}
+                                    </div>
+                                )}
+                            </Field>
+                            <Field name="surname">
+                                {({ input, meta }) => (
+                                    <div>
+                                        <label>Surname</label>
+                                        <input {...input} type="text" placeholder="Password" />
+                                        {meta.error && meta.touched && <span>{meta.error}</span>}
+                                    </div>
+                                )}
+                            </Field>
+                            <Field name="firstdate">
+                                {({ input, meta }) => (
+                                    <div>
+                                        <label>Date of rent</label>
+                                        <input {...input} type="text" placeholder="Confirm" />
+                                        {meta.error && meta.touched && <span>{meta.error}</span>}
+                                    </div>
+                                )}
+                            </Field>
+                            <Field name="seconddate">
+                                {({ input, meta }) => (
+                                    <div>
+                                        <label>Date of give back</label>
+                                        <input {...input} type="password" placeholder="Confirm" />
+                                        {meta.error && meta.touched && <span>{meta.error}</span>}
+                                    </div>
+                                )}
+                            </Field>
+                            <div className="buttons">
+                                <button type="submit" disabled={submitting}>
+                                    Submit
+                                </button>
+                                <button
+                                    type="button"
+                                    onClick={form.reset}
+                                    disabled={submitting || pristine}
+                                >
+                                    Reset
+                                </button>
+                            </div>
+                        </form>
+                    )}
+                />
+            </div>
+        </FormDiv>
 )};
 
 export default RentForm
---
+++ src/CarsToRent/CarsForm/Rent/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const FormDiv = styled.div`
+    width:100%;
+    margin: 5% auto;
+    position: absolute;
+    font-family: 'Economica', sans-serif;
+    font-size:25px;
+`;
